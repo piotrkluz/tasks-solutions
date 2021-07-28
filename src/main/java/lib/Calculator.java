caaaee661a0bc58e5b7cff78e4184872b4fb919c
Piotr Kluz <piotr.kluz@mail.com>
2021-07-28 20:01:23 +0200

    Change implementation - simply use for loop

--- src/main/java/lib/Calculator.java
+++ src/main/java/lib/Calculator.java
@@ -1,8 +1,6 @@
 package lib;
 
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Stream;
 
 import static lib.Utils.reverseList;
@@ -23,17 +21,15 @@ public class Calculator {
         if (invalidNumber.isPresent())
             throw new CalculatorException("Every element should be in range: 0-9. FOUND: " + invalidNumber.get(), input);
 
-        AccumulatorList result = reverseList(Arrays.asList(input)).stream()
-                .reduce(new AccumulatorList(), (acc, curr) -> {
-                    int addValue = acc.moveNext ? curr + 2 : curr + 1;
-                    acc.moveNext = addValue > 9;
-                    acc.add(addValue > 9 ? addValue - 10 : addValue);
+        boolean moveNext = false;
+        List<Integer> result = new ArrayList<>();
+        for(int curr : reverseList(Arrays.asList(input))) {
+            int addValue = moveNext ? curr + 2 : curr + 1;
+            moveNext = addValue > 9;
+            result.add(addValue > 9 ? addValue - 10 : addValue);
+        }
+        if (moveNext) result.add(1);
 
-                    return acc;
-
-                }, (a, b) -> a);
-
-        if (result.moveNext) result.add(1);
         return reverseList(result).toArray(Integer[]::new);
     }
 }
